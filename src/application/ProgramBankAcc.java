693cd5890f744819edcf5d9a021930d4738c517d
Gustavo Gabriel <Gustavo.gfilho2@gmail.com>
2026-03-19 13:25:37 -0300

    Update ProgramBankAcc.java

--- src/application/ProgramBankAcc.java
+++ src/application/ProgramBankAcc.java
@@ -3,9 +3,9 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.BankAcc;
+import entities.BankAccount;
 
-public class ProgramBankAccount {
+public class ProgramBankAcc {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
