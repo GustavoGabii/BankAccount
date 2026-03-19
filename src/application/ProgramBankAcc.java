1a37dcd499d60c0664eb51f09efdda96f32d3809
Gustavo Gabriel <Gustavo.gfilho2@gmail.com>
2026-03-19 13:23:58 -0300

    Update and rename ProgramBankAccount to ProgramBankAcc.java

--- src/application/ProgramBankAcc.java
+++ src/application/ProgramBankAcc.java
@@ -3,7 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.BankAccount;
+import entities.BankAcc;
 
 public class ProgramBankAccount {
 	public static void main(String[] args) {
